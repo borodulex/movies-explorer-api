8f048606010870df846682b9dcef44622e353307
Ilya Borodulin <borodulinid@gmail.com>
2021-08-01 10:33:35 +0300

    feat: creating schemas and models of API resources [+] mongoose [+] validator for schemas [+] schemas and modules for users, movies

---
+++ models/user.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    validate: {
+      validator(v) {
+        return validator.isEmail(v);
+      },
+    },
+  },
+  password: {
+    type: String,
+    required: true,
+    select: false,
+  },
+  name: {
+    type: String,
+    minLength: 2,
+    maxLength: 30,
+  },
+});
+
+module.exports = mongoose.model('user', userSchema);
