ccae3bae3357c8a2a24a05bbba310dcc76076c65
Ilya Borodulin <borodulinid@gmail.com>
2021-08-02 18:53:28 +0300

    feat: non-existent resource routes error handle

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,8 @@ const userRouter = require('./users');
 const movieRouter = require('./movies');
 const auth = require('../middlewares/auth');
 
+const NotFoundError = require('../errors/not-found-err');
+
 const {
   createUser,
   signin,
@@ -15,5 +17,6 @@ router.use(auth);
 router.use('/users', userRouter);
 router.use('/movies', movieRouter);
 router.post('/signout', signout);
+router.use((req, res, next) => next(new NotFoundError('Ресурс не найден. Проверьте URL и метод запроса.')));
 
 module.exports = router;
