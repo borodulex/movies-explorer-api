7b201ecbf6d2f7a965e9c8ce52b039126577ba5c
Ilya Borodulin <borodulinid@gmail.com>
2021-08-03 14:38:10 +0300

    feat: rate-limitter

--- app.js
+++ app.js
@@ -3,9 +3,10 @@ const helmet = require('helmet');
 const mongoose = require('mongoose');
 const { errors } = require('celebrate');
 const cookieParser = require('cookie-parser');
+const limiter = require('./middlewares/rate-limiter');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
-const routes = require('./routes/index');
 const errorHandler = require('./middlewares/error-handler');
+const routes = require('./routes/index');
 
 const { PORT, MONGO_URL } = require('./config');
 
@@ -18,6 +19,7 @@ mongoose.connect(MONGO_URL, {
 });
 
 app.use(requestLogger);
+app.use(limiter);
 app.use(helmet());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
