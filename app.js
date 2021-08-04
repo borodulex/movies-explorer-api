074157711617f92a5b8e22b957c92d33a2124d4c
Ilya Borodulin <borodulinid@gmail.com>
2021-08-04 14:12:22 +0300

    feat: connect router to /api path

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-app.use(routes);
+app.use('/api', routes);
 
 app.use(errorLogger);
 app.use(errors());
